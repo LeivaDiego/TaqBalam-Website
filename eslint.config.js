5a1ac2b32769fc7eae8a5584ac867ad0db6fc83f
Diego Leiva <lei21752@uvg.edu.gt>
2025-09-14 19:27:59 -0600

    chore: prettier improved formatting

--- eslint.config.js
+++ eslint.config.js
@@ -6,27 +6,27 @@ import eslintConfigPrettier from 'eslint-config-prettier/flat'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
+    globalIgnores(['dist']),
+    {
+        files: ['**/*.{js,jsx}'],
+        extends: [
+            js.configs.recommended,
+            reactHooks.configs['recommended-latest'],
+            reactRefresh.configs.vite,
+        ],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+            parserOptions: {
+                ecmaVersion: 'latest',
+                ecmaFeatures: { jsx: true },
+                sourceType: 'module',
+            },
+        },
+        rules: {
+            'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+        },
     },
-    rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-    },
-  },
 
-  eslintConfigPrettier,
+    eslintConfigPrettier,
 ])
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath, URL } from 'node:url'
 
 // Import other plugins as needed
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+    plugins: [react(), tailwindcss()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+            '@Components': fileURLToPath(new URL('./src/components', import.meta.url)),
+            '@UI': fileURLToPath(new URL('./src/components/ui', import.meta.url)),
+            '@Navigation': fileURLToPath(new URL('./src/components/navigation', import.meta.url)),
+            '@Assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
+            '@Lib': fileURLToPath(new URL('./src/lib', import.meta.url)),
+        },
+    },
 })
